11f3a89180e18c907a03c4a67c0a226a20b5d0ad
Picafres4 <al02921139@tecmilenio.mx>
2024-04-09 21:25:43 -0600

    ya 4 pantallas

---
+++ TinyTask/app/src/main/java/com/example/tinytask/newTask.java
@@ -0,0 +1,14 @@
+package com.example.tinytask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
